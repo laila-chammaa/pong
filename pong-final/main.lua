46c9bc495cb905cef46ed20b4123fe58ef190eda
Laila Chammaa <laylachammaa@gmail.com>
2021-12-20 12:08:35 +0300

    AI paddle for a1

--- pong-final/main.lua
+++ pong-final/main.lua
@@ -47,8 +47,10 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
--- paddle movement speed
+-- human paddle movement speed
 PADDLE_SPEED = 200
+
+AI_PADDLE_SPEED = 130
 
 --[[
     Called just once at the beginning of the game; used to set up
@@ -229,15 +231,20 @@ function love.update(dt)
     -- paddles can move no matter what state we're in
     --
     -- player 1
-    if love.keyboard.isDown('w') then
-        player1.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('s') then
-        player1.dy = PADDLE_SPEED
+    -- AI player 1
+    if ball.x < VIRTUAL_WIDTH/2 then
+        if ball.y < player1.y then
+            player1.dy = -AI_PADDLE_SPEED
+        elseif ball.y > player1.y then
+            player1.dy = AI_PADDLE_SPEED
+        else
+            player1.dy = 0
+        end
     else
         player1.dy = 0
     end
 
-    -- player 2
+    -- Human player 2
     if love.keyboard.isDown('up') then
         player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
